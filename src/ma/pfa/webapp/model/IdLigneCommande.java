eaa5528aa90bc683322ef200661eda49f0026a43
Soukaina <souka.bah@gmail.com>
2019-03-07 16:24:59 +0100

    updated IdLigneCommande

--- src/ma/pfa/webapp/model/IdLigneCommande.java
+++ src/ma/pfa/webapp/model/IdLigneCommande.java
@@ -1,5 +1,46 @@
 package ma.pfa.webapp.model;
 
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+
 public class IdLigneCommande {
+	@ManyToOne
+	@JoinColumn(name="id_produit")
+	private Produit produit;
+	
+	@ManyToOne
+	@JoinColumn(name="id_commande")
+	private CommandeClient commandeClient;
+
+	
+	
+	public IdLigneCommande() {
+		super();
+	}
+
+	public IdLigneCommande(Produit produit, CommandeClient commandeClient) {
+		super();
+		this.produit = produit;
+		this.commandeClient = commandeClient;
+	}
+
+	public Produit getProduit() {
+		return produit;
+	}
+
+	public void setProduit(Produit produit) {
+		this.produit = produit;
+	}
+
+	public CommandeClient getCommandeClient() {
+		return commandeClient;
+	}
+
+	public void setCommandeClient(CommandeClient commandeClient) {
+		this.commandeClient = commandeClient;
+	}
 
 }
