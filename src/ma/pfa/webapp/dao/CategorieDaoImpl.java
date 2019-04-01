00580e474b21ff72b52b4c521ccfc08465cc897b
I. Saber <isaber23@outlook.com>
2019-04-01 18:31:12 +0100

    added categorie dao

---
+++ src/ma/pfa/webapp/dao/CategorieDaoImpl.java
@@ -0,0 +1,9 @@
+package ma.pfa.webapp.dao;
+
+import ma.pfa.webapp.model.Categorie;
+
+public class CategorieDaoImpl extends CrudGenericDaoImpl<Categorie> implements ICategorieDao {
+
+
+
+}
---
+++ src/ma/pfa/webapp/dao/ICategorieDao.java
@@ -0,0 +1,7 @@
+package ma.pfa.webapp.dao;
+
+import ma.pfa.webapp.model.Categorie;
+
+public interface ICategorieDao extends ICrudGenericDao<Categorie>{
+
+}
