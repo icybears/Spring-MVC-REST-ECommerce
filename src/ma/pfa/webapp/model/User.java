def3f4c4ccbfe8c58bd7a14806b6c604a2876d06
I. Saber <isaber23@outlook.com>
2019-05-01 14:22:09 +0100

    testing

--- src/ma/pfa/webapp/model/User.java
+++ src/ma/pfa/webapp/model/User.java
@@ -17,7 +17,7 @@ public class User {
 	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
 	private UserRole userRole;
 
-	private String userName;
+	private String email;
 
 	private String password;
 	
@@ -25,9 +25,9 @@ public class User {
 		super();
 	}
 	
-	public User(String userName, String password) {
+	public User(String email, String password) {
 		super();
-		this.userName = userName;
+		this.email = email;
 		this.password = password;
 	}
 
@@ -39,8 +39,8 @@ public class User {
 		return password;
 	}
 
-	public String getUserName() {
-		return userName;
+	public String getEmail() {
+		return email;
 	}
 
 	public UserRole getUserRole() {
@@ -55,8 +55,8 @@ public class User {
 		this.password = password;
 	}
 
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public void setUserRole(UserRole userRole) {
